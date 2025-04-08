d256d71fe6916ebec10b4d5b60b5087fa0483be1
Mohith Reddy <smohithreddy00@gmail.com>
2025-04-08 07:18:28 +0530

    🦾 Updated contact section and added Iron Man theme

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -142,7 +142,7 @@ const projects = [
       "https://images.unsplash.com/photo-1626682561113-d1db402cc866?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     technologies: ["React", "Cloudinary", "Express", "MongoDB"],
     github: "https://github.com",
-    live: "https://example.com",
+    live: "https://qr-code-generator-orcin-three.vercel.app/",
   },
 ];
 
